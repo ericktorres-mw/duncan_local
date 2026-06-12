7570e5c149443691d902086340de126e5e0941be
Erick Torres <erick.torres@midware.net>
2026-06-12 14:59:01 +0000

    fix(sync): Manual sync update
    This commit was generated by a manual sync action in Cycle.
    - Added/Updated 1 file(s) from NetSuite.
    - Deleted 0 file(s) from the repository.
    
    Added/Updated files:
    - SuiteScripts/SDFTest.js
    
    Deleted files:
    
    
    Generated with [Cycle](https://cycle.sh)
    Co-Authored-By: Erick Torres <erick.torres@midware.net>

--- SuiteScripts/SDFTest.js
+++ SuiteScripts/SDFTest.js
@@ -4,6 +4,3 @@ test after fix 1 and db migration
 // envlock main #1 @055107
 // envlock main #2 @055107
 // envlock main #3 @055107
-// serial-test #1 @062351
-// serial-test #2 @062351
-// serial-test #3 @062351
